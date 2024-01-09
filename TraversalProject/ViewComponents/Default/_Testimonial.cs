8ab230986d1037117cce323a615169cbf2497d16
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-01-09 05:16:37 +0300

    completed

--- TraversalProject/ViewComponents/Default/_Testimonial.cs
+++ TraversalProject/ViewComponents/Default/_Testimonial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalProject.ViewComponents.Default
 {
     public class _Testimonial:ViewComponent
     {
+        TestimonialManager testimonialManager = new(new EfTestimonialDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = testimonialManager.TGetlist();
+            return View(values);
         }
     }
 }
