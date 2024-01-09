da7abe817445190fca665887bcb04c688cde146e
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-01-09 23:05:18 +0300

    lesson 26 completed

---
+++ TraversalProject/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new(new EfCommentDal());
+
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult AddComment(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentState = true;
+            p.DestinationId = 3;
+            commentManager.Tadd(p);
+            return RedirectToAction("Index","Destination");
+        }
+    }
+}
